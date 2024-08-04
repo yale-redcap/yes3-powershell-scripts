90ff8dbc22ffeaddb5997b9a0074e4093692637f
Peter Charpentier <65468174+criwebtools@users.noreply.github.com>
2024-08-03 21:43:56 -0400

    Update session-start.ps1

--- session-start.ps1
+++ session-start.ps1
@@ -1,5 +1,5 @@
 # Import the common functions
-. "$env:USERPROFILE\Documents\WindowsPowerShell\session-functions.ps1"
+. .\session-functions.ps1
 
 # Get the session branch environment variable name
 $envVarName = Get-SessionBranchEnvVarName
